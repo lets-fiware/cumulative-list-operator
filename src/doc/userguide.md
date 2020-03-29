abb2449178bcb3ad3e327a3826abae6ca9d6ebd3
Kazuhito Suda <kazuhito@fisuda.jp>
2020-03-29 21:07:00 +0900

    first commit

---
+++ src/doc/userguide.md
@@ -0,0 +1,17 @@
+## Introduction
+
+calc cumulative operator
+
+## Settings
+
+## Wiring
+
+### Input Endpoints
+
+### Output Endpoints
+
+## Usage
+
+## Reference
+
+- [FIWARE Mashup](https://mashup.lab.fiware.org/)
---
+++ src/js/main.js
@@ -0,0 +1,60 @@
+/*
+ * calc-cumulative
+ * https://github.com/lets-fiware/cumulative-list-operator
+ *
+ * Copyright (c) 2020 Kazuhito Suda
+ * Licensed under the MIT license.
+ */
+
+(function () {
+
+    "use strict";
+
+    var parseInputEndpointData = function parseInputEndpointData(data) {
+        if (typeof data === "string") {
+            try {
+                data = JSON.parse(data);
+            } catch (e) {
+                throw new MashupPlatform.wiring.EndpointTypeError();
+            }
+        }
+
+        if (data != null && !Array.isArray(data)) {
+            throw new MashupPlatform.wiring.EndpointTypeError();
+        }
+
+        return data;
+    };
+
+    var valueList = function valueList(indata) {
+        indata = parseInputEndpointData(indata);
+
+        var send_nulls = MashupPlatform.prefs.get("send_nulls");
+        if (indata == null && send_nulls) {
+            return MashupPlatform.wiring.pushEvent('outdata', null);
+        } else if (indata == null) {
+            return; // do nothing
+        }
+
+        var sum = 0;
+        var outdata = indata.map(data => {
+            if (data == null) {
+                return null;
+            }
+            sum += data;
+            return sum;
+        });
+        MashupPlatform.wiring.pushEvent("outdata", outdata);
+    };
+
+    /* TODO
+     * this if is required for testing, but we have to search a cleaner way
+     */
+    if (window.MashupPlatform != null) {
+        MashupPlatform.wiring.registerCallback("indata", valueList);
+    }
+
+    /* test-code */
+    window.valueList = valueList;
+    /* end-test-code */
+})();
